d388ba7450adea91aac85982ce1dace155784df1
JoaoF2025 <joaolmf@ua.pt>
2023-09-18 18:01:26 +0100

    First E0 log and some new features python script

--- Temperature Logger/Temperature_Logger.py
+++ Temperature Logger/Temperature_Logger.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 # Define the serial port and baud rate (change this to match your setup)
-serial_port = '/dev/ttyUSB0'  # Change this to your specific serial port (Windows: 'COMx')
+serial_port = '/dev/ttyACM0'  # Change this to your specific serial port (Windows: 'COMx')
 baud_rate = 115200  # Change this to match your printer's baud rate
 
 # Function to send a G-code command and receive the response
@@ -16,14 +16,21 @@ def send_gcode(command, printer):
     response = printer.readline().decode().strip()
     return response
 
+# Function to set the hotend temperature to the desired value
+def set_hotend_temperature(desired_temperature, printer):
+    # Enforce temperature limits (30°C to 251°C)
+    desired_temperature = max(30, min(251, desired_temperature))
+    
+    # Set the hotend temperature to the desired value
+    hotend_set_temp_command = f"M104 S{desired_temperature}\n"
+    send_gcode(hotend_set_temp_command, printer)
+    
+    print(f"Hotend temperature set to {desired_temperature} °C")
+
 # Function to record hotend temperature for a duration
 def record_temperature(duration_seconds, output_file, printer):
     start_time = datetime.now()
     end_time = start_time + timedelta(seconds=duration_seconds)
-
-    # Set the hotend temperature to 250°C at the beginning
-    hotend_set_temp_command = "M104 S250\n"  # Set hotend temperature to 250°C
-    send_gcode(hotend_set_temp_command, printer)
 
     data = []
 
@@ -49,7 +56,7 @@ def record_temperature(duration_seconds, output_file, printer):
         else:
             print(f"Elapsed Time (s): {elapsed_seconds}, Failed to retrieve hotend temperature.")
 
-        # Wait for 0.8 seconds before the next measurement
+        # Wait for 1 second before the next measurement
         time.sleep(0.8)
 
     # Turn off the hotend at the end
@@ -69,8 +76,14 @@ try:
     # Open the serial connection
     printer = serial.Serial(serial_port, baud_rate, timeout=2)
 
+    # Ask the user for the desired hotend temperature
+    desired_temperature = float(input("Enter the desired hotend temperature (30°C to 251°C): "))
+    
+    # Set the hotend temperature to the desired value
+    set_hotend_temperature(desired_temperature, printer)
+
     with pd.ExcelWriter(output_file_name, engine="xlsxwriter") as output_file:
-        record_duration_seconds = 300
+        record_duration_seconds = 300  # Change this to the desired duration in seconds
         record_temperature(record_duration_seconds, output_file, printer)
 
     # Close the serial connection
@@ -80,3 +93,5 @@ except serial.SerialException:
     print(f"Failed to open serial port {serial_port}. Make sure the printer is connected.")
 except KeyboardInterrupt:
     print("Recording stopped by user.")
+except ValueError:
+    print("Invalid input for desired temperature. Please enter a valid number between 30°C and 251°C.")
